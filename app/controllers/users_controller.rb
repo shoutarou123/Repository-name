5b90fad1794544754ec4092ca1d90d365201f4b7
将太朗 <850123.a@gmail.com>
2023-10-15 09:44:46 +0000

    9.3.1終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -44,6 +44,12 @@ class UsersController < ApplicationController
     redirect_to users_url
   end
 
+  def edit_basic_info
+  end
+
+  def update_basic_info
+  end
+
   private
 
     def user_params
@@ -59,11 +65,11 @@ class UsersController < ApplicationController
 
     # ログイン済みのユーザーか確認します。
     def logged_in_user
-        unless logged_in?
-          store_location
-          flash[:danger] = "ログインしてください。"
-          redirect_to login_url
-        end
+      unless logged_in?
+        store_location
+        flash[:danger] = "ログインしてください。"
+        redirect_to login_url
+      end
     end
 
     # アクセスしたユーザーが現在ログインしているユーザーか確認します。
